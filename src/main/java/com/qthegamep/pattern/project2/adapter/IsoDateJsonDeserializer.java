556bd278d13a1d19d9eaceca58aebcc8cb4e16f2
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-09-06 16:04:31 +0300

    Created IsoDateJsonDeserializer class

---
+++ src/main/java/com/qthegamep/pattern/project2/adapter/IsoDateJsonDeserializer.java
@@ -0,0 +1,27 @@
+package com.qthegamep.pattern.project2.adapter;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.qthegamep.pattern.project2.util.Constants;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class IsoDateJsonDeserializer extends JsonDeserializer<Date> {
+
+    @Override
+    public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constants.MONGO_UTC_DATE_FORMAT.getValue());
+            JsonNode jsonNode = jsonParser.readValueAsTree();
+            String isoDate = jsonNode.get("$date").asText();
+            return simpleDateFormat.parse(isoDate);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+}
