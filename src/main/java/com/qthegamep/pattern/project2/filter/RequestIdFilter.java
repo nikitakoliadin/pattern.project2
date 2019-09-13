3ab2c4e5c7235fc213c17af326e1a1a4c25cf971
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-09-13 10:35:18 +0300

    Updated logic of generating request id

--- src/main/java/com/qthegamep/pattern/project2/filter/RequestIdFilter.java
+++ src/main/java/com/qthegamep/pattern/project2/filter/RequestIdFilter.java
@@ -1,15 +1,16 @@
 package com.qthegamep.pattern.project2.filter;
 
+import com.qthegamep.pattern.project2.service.GenerationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Priority;
+import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
-import java.security.SecureRandom;
 
 @Provider
 @PreMatching
@@ -18,27 +19,20 @@ public class RequestIdFilter implements ContainerRequestFilter {
 
     private static final Logger LOG = LoggerFactory.getLogger(RequestIdFilter.class);
 
+    private GenerationService generationService;
+
+    @Inject
+    public RequestIdFilter(GenerationService generationService) {
+        this.generationService = generationService;
+    }
+
     @Override
     public void filter(ContainerRequestContext containerRequestContext) {
         String requestId = containerRequestContext.getHeaderString("x-request-id") == null
-                ? generateRequestId(10)
+                ? generationService.generateUniqueId(10L)
                 : containerRequestContext.getHeaderString("x-request-id");
         LOG.debug("RequestId: {}", requestId);
         MultivaluedMap<String, String> headers = containerRequestContext.getHeaders();
         headers.add("requestId", requestId);
-    }
-
-    private static String generateRequestId(int length) {
-        StringBuilder result = new StringBuilder();
-        SecureRandom secureRandom = new SecureRandom();
-        for (int i = 0; i < length; ++i) {
-            boolean upperCase = secureRandom.nextBoolean();
-            char symbol = (char) (secureRandom.nextInt(26) + 97);
-            if (upperCase) {
-                symbol = Character.toUpperCase(symbol);
-            }
-            result.append(symbol);
-        }
-        return result.toString();
     }
 }
