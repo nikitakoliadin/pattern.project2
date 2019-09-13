0af5ce3f7dcecfe1b7b683a1b3b4cee9d4eee5b9
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-09-13 10:32:50 +0300

    Implements ApplicationBinder logic

--- src/main/java/com/qthegamep/pattern/project2/binder/ApplicationBinder.java
+++ src/main/java/com/qthegamep/pattern/project2/binder/ApplicationBinder.java
@@ -1,11 +1,52 @@
 package com.qthegamep.pattern.project2.binder;
 
+import com.qthegamep.pattern.project2.service.GenerationService;
+import com.qthegamep.pattern.project2.service.GenerationServiceImpl;
 import org.glassfish.jersey.internal.inject.AbstractBinder;
+
+import javax.inject.Singleton;
+import java.util.Objects;
 
 public class ApplicationBinder extends AbstractBinder {
 
+    private GenerationService generationService;
+
+    private ApplicationBinder(GenerationService generationService) {
+        this.generationService = generationService;
+    }
+
+    public GenerationService getGenerationService() {
+        return generationService;
+    }
+
+    public static ApplicationBinderBuilder builder() {
+        return new ApplicationBinderBuilder();
+    }
+
     @Override
     protected void configure() {
-        // TODO: Write your injections here
+        bindGenerationService();
+    }
+
+    private void bindGenerationService() {
+        if (Objects.isNull(generationService)) {
+            bind(GenerationServiceImpl.class).to(GenerationService.class).in(Singleton.class);
+        } else {
+            bind(generationService).to(GenerationService.class).in(Singleton.class);
+        }
+    }
+
+    public static class ApplicationBinderBuilder {
+
+        private GenerationService generationService;
+
+        public ApplicationBinderBuilder setGenerationService(GenerationService generationService) {
+            this.generationService = generationService;
+            return this;
+        }
+
+        public ApplicationBinder build() {
+            return new ApplicationBinder(generationService);
+        }
     }
 }
