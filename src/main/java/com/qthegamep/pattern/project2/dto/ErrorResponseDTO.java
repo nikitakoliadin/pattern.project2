5343b10436c91d72e136e49f7fe0534426ab39e2
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-09-13 08:47:41 +0300

    Added Swagger to ErrorResponseDTO

--- src/main/java/com/qthegamep/pattern/project2/dto/ErrorResponseDTO.java
+++ src/main/java/com/qthegamep/pattern/project2/dto/ErrorResponseDTO.java
@@ -2,15 +2,25 @@ package com.qthegamep.pattern.project2.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.qthegamep.pattern.project2.model.ResponseCode;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.Objects;
 
+@Schema(name = "Error",
+        description = "This is error response object")
 public class ErrorResponseDTO {
 
+    @Schema(description = "Request ID",
+            required = true,
+            example = "WYAtQAwLqb")
     @JsonProperty(
             value = "requestId",
             required = true)
     private String requestId;
+
+    @Schema(description = "Response code",
+            required = true,
+            example = "INTERNAL_ERROR")
     @JsonProperty(
             value = "responseCode",
             required = true)
