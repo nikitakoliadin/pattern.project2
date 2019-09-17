f7e277946d2024bddef992650090f6448c23b4a7
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-09-17 08:42:41 +0300

    Added ConverterService to binding

--- src/main/java/com/qthegamep/pattern/project2/binder/ApplicationBinder.java
+++ src/main/java/com/qthegamep/pattern/project2/binder/ApplicationBinder.java
@@ -2,6 +2,8 @@ package com.qthegamep.pattern.project2.binder;
 
 import com.qthegamep.pattern.project2.service.GenerationService;
 import com.qthegamep.pattern.project2.service.GenerationServiceImpl;
+import com.qthegamep.pattern.project2.service.ConverterService;
+import com.qthegamep.pattern.project2.service.ConverterServiceImpl;
 import org.glassfish.jersey.internal.inject.AbstractBinder;
 
 import javax.inject.Singleton;
@@ -9,10 +11,16 @@ import java.util.Objects;
 
 public class ApplicationBinder extends AbstractBinder {
 
+    private ConverterService converterService;
     private GenerationService generationService;
 
-    private ApplicationBinder(GenerationService generationService) {
+    private ApplicationBinder(ConverterService converterService, GenerationService generationService) {
+        this.converterService = converterService;
         this.generationService = generationService;
+    }
+
+    public ConverterService getConverterService() {
+        return converterService;
     }
 
     public GenerationService getGenerationService() {
@@ -25,7 +33,16 @@ public class ApplicationBinder extends AbstractBinder {
 
     @Override
     protected void configure() {
+        bindConverterService();
         bindGenerationService();
+    }
+
+    private void bindConverterService() {
+        if (Objects.isNull(converterService)) {
+            bind(ConverterServiceImpl.class).to(ConverterService.class).in(Singleton.class);
+        } else {
+            bind(converterService).to(ConverterService.class).in(Singleton.class);
+        }
     }
 
     private void bindGenerationService() {
@@ -38,7 +55,13 @@ public class ApplicationBinder extends AbstractBinder {
 
     public static class ApplicationBinderBuilder {
 
+        private ConverterService converterService;
         private GenerationService generationService;
+
+        public ApplicationBinderBuilder setConverterService(ConverterService converterService) {
+            this.converterService = converterService;
+            return this;
+        }
 
         public ApplicationBinderBuilder setGenerationService(GenerationService generationService) {
             this.generationService = generationService;
@@ -46,7 +69,7 @@ public class ApplicationBinder extends AbstractBinder {
         }
 
         public ApplicationBinder build() {
-            return new ApplicationBinder(generationService);
+            return new ApplicationBinder(converterService, generationService);
         }
     }
 }
