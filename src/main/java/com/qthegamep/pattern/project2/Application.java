1c63a34554caafd78f7c4281a85de3c2dd720b24
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-10-31 17:02:27 +0200

    Updated Application class

--- src/main/java/com/qthegamep/pattern/project2/Application.java
+++ src/main/java/com/qthegamep/pattern/project2/Application.java
@@ -2,8 +2,16 @@ package com.qthegamep.pattern.project2;
 
 import com.qthegamep.pattern.project2.binder.ApplicationBinder;
 import com.qthegamep.pattern.project2.config.ApplicationConfig;
-import com.qthegamep.pattern.project2.exception.ApplicationConfigException;
+import com.qthegamep.pattern.project2.config.IOStrategyFactory;
+import com.qthegamep.pattern.project2.config.TaskQueueSizeLogProbe;
+import com.qthegamep.pattern.project2.exception.ApplicationConfigInitializationException;
+import com.qthegamep.pattern.project2.model.IoStrategyType;
+import com.qthegamep.pattern.project2.util.Constants;
 import org.glassfish.grizzly.http.server.*;
+import org.glassfish.grizzly.monitoring.MonitoringConfig;
+import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
+import org.glassfish.grizzly.threadpool.ThreadPoolConfig;
+import org.glassfish.grizzly.threadpool.ThreadPoolProbe;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
@@ -15,7 +23,7 @@ public class Application {
 
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) throws ApplicationConfigException {
+    public static void main(String[] args) throws ApplicationConfigInitializationException {
         initConfigs();
         String host = System.getProperty("application.host", "0.0.0.0");
         String port = System.getProperty("application.port", "8080");
@@ -26,7 +34,7 @@ public class Application {
         addSwaggerUIMapping(httpServer, applicationContext + swaggerUrl);
     }
 
-    private static void initConfigs() throws ApplicationConfigException {
+    private static void initConfigs() throws ApplicationConfigInitializationException {
         ApplicationConfig applicationConfig = new ApplicationConfig();
         applicationConfig.init();
     }
@@ -36,9 +44,44 @@ public class Application {
         ResourceConfig resourceConfig = new ResourceConfig()
                 .packages(Application.class.getPackage().getName())
                 .register(ApplicationBinder.builder().build());
-        HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(applicationUri, resourceConfig);
-        LOG.info("{} application started at {}", Application.class.getPackage().getName(), applicationUrl);
+        HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(applicationUri, resourceConfig, false);
+        configServer(httpServer, applicationUrl);
         return httpServer;
+    }
+
+    private static void configServer(HttpServer httpServer, String applicationUrl) {
+        try {
+            ServerConfiguration serverConfiguration = httpServer.getServerConfiguration();
+            serverConfiguration.setJmxEnabled(true);
+            TCPNIOTransport grizzlyTransport = httpServer.getListener(Constants.GRIZZLY.getValue()).getTransport();
+            ThreadPoolConfig threadPoolConfig = ThreadPoolConfig.defaultConfig()
+                    .setPoolName(Constants.GRIZZLY_POOL_NAME.getValue())
+                    .setCorePoolSize(32)
+                    .setMaxPoolSize(32)
+                    .setQueueLimit(-1);
+            grizzlyTransport.configureBlocking(false);
+            grizzlyTransport.setSelectorRunnersCount(Runtime.getRuntime().availableProcessors() * 4);
+            grizzlyTransport.setWorkerThreadPoolConfig(threadPoolConfig);
+            grizzlyTransport.setKernelThreadPoolConfig(threadPoolConfig);
+            grizzlyTransport.setIOStrategy(new IOStrategyFactory().createIOStrategy(IoStrategyType.DYNAMIC_IO_STRATEGY));
+            addProbes(grizzlyTransport);
+            httpServer.start();
+            LOG.info("\nBlocking Transport(T/F): {}\nNum SelectorRunners: {}\nNum WorkerThreads: {}\nNum KernelThreadPool: {}\nQueue limit: {}",
+                    grizzlyTransport.isBlocking(),
+                    grizzlyTransport.getSelectorRunnersCount(),
+                    grizzlyTransport.getWorkerThreadPoolConfig().getCorePoolSize(),
+                    grizzlyTransport.getKernelThreadPoolConfig().getCorePoolSize(),
+                    grizzlyTransport.getWorkerThreadPoolConfig().getQueueLimit());
+            LOG.info("{} application started at {}", Application.class.getPackage().getName(), applicationUrl);
+        } catch (Exception e) {
+            LOG.error("Error", e);
+        }
+    }
+
+    private static void addProbes(TCPNIOTransport grizzlyTransport) {
+        MonitoringConfig<ThreadPoolProbe> threadPoolMonitoringConfig = grizzlyTransport.getThreadPoolMonitoringConfig();
+        threadPoolMonitoringConfig.addProbes(new TaskQueueSizeLogProbe());
+        LOG.info("Probes was added");
     }
 
     private static void addSwaggerUIMapping(HttpServer httpServer, String contextPath) {
@@ -52,7 +95,7 @@ public class Application {
                 .build();
         ServerConfiguration serverConfiguration = httpServer.getServerConfiguration();
         serverConfiguration.addHttpHandler(docsStaticHttpHandler, httpHandlerRegistration);
-        NetworkListener grizzlyListener = httpServer.getListener("grizzly");
+        NetworkListener grizzlyListener = httpServer.getListener(Constants.GRIZZLY.getValue());
         String swaggerPath = "http://" + grizzlyListener.getHost() + ":" + grizzlyListener.getPort() + contextPath + urlPattern;
         LOG.info("Swagger openApi available at {}", swaggerPath);
     }
