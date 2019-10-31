bacc4489f8a8a0ceda275eacc98a06133e3a84f1
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-10-31 16:47:21 +0200

    Added UNKNOWN_ERROR type

--- src/main/java/com/qthegamep/pattern/project2/model/ErrorType.java
+++ src/main/java/com/qthegamep/pattern/project2/model/ErrorType.java
@@ -3,7 +3,8 @@ package com.qthegamep.pattern.project2.model;
 public enum ErrorType {
 
     INTERNAL_ERROR(500),
-    OPEN_API_ERROR(501);
+    OPEN_API_ERROR(501),
+    UNKNOWN_ERROR(502);
 
     private int errorCode;
 
