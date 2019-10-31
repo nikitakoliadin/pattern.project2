5610a1d48c321da84b6c0b163cc72ba32dbac914
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-10-31 16:58:37 +0200

    Created IoStrategyType class

---
+++ src/main/java/com/qthegamep/pattern/project2/model/IoStrategyType.java
@@ -0,0 +1,9 @@
+package com.qthegamep.pattern.project2.model;
+
+public enum IoStrategyType {
+
+    SAME_IO_STRATEGY,
+    WORKER_IO_STRATEGY,
+    LEADER_FOLLOWER_IO_STRATEGY,
+    DYNAMIC_IO_STRATEGY;
+}
