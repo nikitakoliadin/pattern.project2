764d975fc66b7d4e40b4e043d13a30a02425c672
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-10-31 16:44:48 +0200

    Created ServiceException

---
+++ src/main/java/com/qthegamep/pattern/project2/exception/ServiceException.java
@@ -0,0 +1,8 @@
+package com.qthegamep.pattern.project2.exception;
+
+import com.qthegamep.pattern.project2.model.ErrorType;
+
+public interface ServiceException {
+
+    ErrorType getErrorType();
+}
