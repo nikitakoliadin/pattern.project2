037021cee68abff0b742c217c616cb66d7699222
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-10-31 16:36:58 +0200

    Updated HealthCheckerController

--- src/main/java/com/qthegamep/pattern/project2/controller/HealthCheckerController.java
+++ src/main/java/com/qthegamep/pattern/project2/controller/HealthCheckerController.java
@@ -1,11 +1,30 @@
 package com.qthegamep.pattern.project2.controller;
 
+import com.qthegamep.pattern.project2.dto.ErrorResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.core.Response;
 
-@FunctionalInterface
+@Tag(name = "Health Checker API")
+@Singleton
 public interface HealthCheckerController {
 
+    @Operation(
+            summary = "Test service status",
+            description = "Check if service is alive")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Service is alive")
+    @ApiResponse(
+            responseCode = "others",
+            description = "Service is dead",
+            content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     @GET
     Response test();
 }
--- src/main/java/com/qthegamep/pattern/project2/controller/HealthCheckerControllerImpl.java
+++ src/main/java/com/qthegamep/pattern/project2/controller/HealthCheckerControllerImpl.java
@@ -1,9 +1,11 @@
 package com.qthegamep.pattern.project2.controller;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+@Singleton
 @Path("/test")
 public class HealthCheckerControllerImpl implements HealthCheckerController {
 
