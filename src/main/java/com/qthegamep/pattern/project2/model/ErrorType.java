6fa9e9ddab6b26da6f145853258e2c54ce55a8d0
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-10-31 16:38:00 +0200

    Updated ErrorType model

---
+++ src/main/java/com/qthegamep/pattern/project2/model/ErrorType.java
@@ -0,0 +1,17 @@
+package com.qthegamep.pattern.project2.model;
+
+public enum ErrorType {
+
+    INTERNAL_ERROR(500),
+    OPEN_API_ERROR(501);
+
+    private int errorCode;
+
+    ErrorType(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+}
--- src/main/java/com/qthegamep/pattern/project2/model/ResponseCode.java
+++
@@ -1,6 +0,0 @@
-package com.qthegamep.pattern.project2.model;
-
-public enum ResponseCode {
-
-    INTERNAL_ERROR
-}
