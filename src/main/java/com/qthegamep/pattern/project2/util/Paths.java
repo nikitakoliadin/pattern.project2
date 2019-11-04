03325fa1aed7280fa0fed5e71f9d92d45e57bc2c
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-04 14:44:46 +0200

    Updated DOCS_PATH const

--- src/main/java/com/qthegamep/pattern/project2/util/Paths.java
+++ src/main/java/com/qthegamep/pattern/project2/util/Paths.java
@@ -4,6 +4,6 @@ public class Paths {
 
     public static final String TEST_PATH = "/test";
     public static final String SWAGGER_PATH = "/swagger";
-    public static final String DOCS_PATH = "/docs";
+    public static final String DOCS_PATH = "/docs/";
     public static final String METRICS_PATH = "/metrics";
 }
