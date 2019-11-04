d8200a9f55ebf2870cc9501b34d483dc18ca2d3a
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-04 14:32:07 +0200

    Updated TaskQueueSizeProbe class

--- src/main/java/com/qthegamep/pattern/project2/config/TaskQueueSizeProbe.java
+++ src/main/java/com/qthegamep/pattern/project2/config/TaskQueueSizeProbe.java
@@ -7,15 +7,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Queue;
+import java.util.concurrent.atomic.AtomicLong;
 
-public class TaskQueueSizeLogProbe extends ThreadPoolProbe.Adapter {
+public class TaskQueueSizeProbe extends ThreadPoolProbe.Adapter {
 
-    private static final Logger LOG = LoggerFactory.getLogger(TaskQueueSizeLogProbe.class);
+    private static final Logger LOG = LoggerFactory.getLogger(TaskQueueSizeProbe.class);
+
+    public static final AtomicLong TASK_QUEUE_SIZE = new AtomicLong();
 
     @Override
     public void onTaskQueueEvent(AbstractThreadPool threadPool, Runnable task) {
         ThreadPoolConfig threadPoolConfig = threadPool.getConfig();
         Queue<Runnable> taskQueue = threadPoolConfig.getQueue();
-        LOG.info("--->>> New task in queue. Queue size: {}", taskQueue.size());
+        LOG.info("--->>> New task in queue. Task queue size: {}", taskQueue.size());
+        TASK_QUEUE_SIZE.set(taskQueue.size());
+        LOG.debug("Atomic task queue size: {}", TASK_QUEUE_SIZE.get());
     }
 }
