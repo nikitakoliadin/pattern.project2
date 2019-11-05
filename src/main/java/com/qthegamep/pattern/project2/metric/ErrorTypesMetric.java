f795bdf5e08e206d2bd3c05ac724a0dfe3a4a5ef
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-05 17:22:21 +0200

    Created ErrorTypesMetric class

---
+++ src/main/java/com/qthegamep/pattern/project2/metric/ErrorTypesMetric.java
@@ -0,0 +1,26 @@
+package com.qthegamep.pattern.project2.metric;
+
+import com.qthegamep.pattern.project2.util.Constants;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import sun.management.Util;
+
+import java.lang.management.PlatformManagedObject;
+
+public class ErrorTypesMetric implements MeterBinder {
+
+    private static final String ERROR_TYPES = "error.types";
+    private static final String ERROR_CODE_TAG = "error.code";
+
+    @Override
+    public void bindTo(MeterRegistry meterRegistry) {
+        PlatformManagedObject errorTypesPlatformManagedObject = () -> Util.newObjectName("error.types");
+        Metrics.ERROR_TYPES_METRIC.forEach((key, value) -> Gauge.builder(ERROR_TYPES, errorTypesPlatformManagedObject, (platformManagedObject) -> value.get())
+                .description("The error response types")
+                .baseUnit(Constants.GRIZZLY.getValue())
+                .tags(Tags.of(ERROR_CODE_TAG, key))
+                .register(meterRegistry));
+    }
+}
