660933f014ab4a9689f4cbcb8afe17fe8d651d7b
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-05 13:16:14 +0200

    Created Metrics class

---
+++ src/main/java/com/qthegamep/pattern/project2/metrics/Metrics.java
@@ -0,0 +1,8 @@
+package com.qthegamep.pattern.project2.metrics;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Metrics {
+
+    public static final AtomicLong TASK_QUEUE_SIZE_METRIC = new AtomicLong();
+}
