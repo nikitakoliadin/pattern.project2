589ecde6b68ce47db246424da110054c8e5b829f
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-05 13:17:17 +0200

    Updated TaskQueueSizeMetrics class

--- src/main/java/com/qthegamep/pattern/project2/metrics/TaskQueueSizeMetrics.java
+++ src/main/java/com/qthegamep/pattern/project2/metrics/TaskQueueSizeMetrics.java
@@ -1,20 +1,23 @@
 package com.qthegamep.pattern.project2.metrics;
 
-import com.qthegamep.pattern.project2.config.TaskQueueSizeProbe;
 import com.qthegamep.pattern.project2.util.Constants;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 public class TaskQueueSizeMetrics implements MeterBinder {
+
+    private static final String TASK_QUEUE_SIZE = "task.queue.size";
+    private static final String TASK_QUEUE_LIMIT = "task.queue.limit";
 
     @Override
     public void bindTo(MeterRegistry meterRegistry) {
-        AtomicLong taskQueueSize = TaskQueueSizeProbe.TASK_QUEUE_SIZE;
-        Gauge.builder(Constants.TASK_QUEUE_SIZE_METRICS.getValue(), taskQueueSize::get)
+        Gauge.builder(TASK_QUEUE_SIZE, Metrics.TASK_QUEUE_SIZE_METRIC::get)
                 .description("The current number of tasks in queue")
+                .baseUnit(Constants.GRIZZLY.getValue())
+                .register(meterRegistry);
+        Gauge.builder(TASK_QUEUE_LIMIT, () -> Integer.parseInt(System.getProperty("server.queue.limit")))
+                .description("The limit size of the task queue. If limit value is negative then queue has infinity limit")
                 .baseUnit(Constants.GRIZZLY.getValue())
                 .register(meterRegistry);
     }
