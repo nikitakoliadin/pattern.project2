894ef3069e4475424b080280fdf1eefd6cf541dd
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-05 13:20:02 +0200

    Added AVAILABLE_THREADS_METRIC

--- src/main/java/com/qthegamep/pattern/project2/metrics/Metrics.java
+++ src/main/java/com/qthegamep/pattern/project2/metrics/Metrics.java
@@ -5,4 +5,6 @@ import java.util.concurrent.atomic.AtomicLong;
 public class Metrics {
 
     public static final AtomicLong TASK_QUEUE_SIZE_METRIC = new AtomicLong();
+
+    public static final AtomicLong AVAILABLE_THREADS_METRIC = new AtomicLong(Integer.parseInt(System.getProperty("server.core.pool.size")));
 }
