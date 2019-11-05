f673edfba30dff17bcea497b8e334c25d672c5e0
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-05 17:24:14 +0200

    Register error types metric

--- src/main/java/com/qthegamep/pattern/project2/mapper/GeneralExceptionMapper.java
+++ src/main/java/com/qthegamep/pattern/project2/mapper/GeneralExceptionMapper.java
@@ -2,6 +2,7 @@ package com.qthegamep.pattern.project2.mapper;
 
 import com.qthegamep.pattern.project2.dto.ErrorResponseDTO;
 import com.qthegamep.pattern.project2.exception.ServiceException;
+import com.qthegamep.pattern.project2.metric.Metrics;
 import com.qthegamep.pattern.project2.model.ErrorType;
 import com.qthegamep.pattern.project2.service.ErrorResponseBuilderService;
 import com.qthegamep.pattern.project2.util.Constants;
@@ -42,6 +43,7 @@ public class GeneralExceptionMapper implements ExceptionMapper<Exception> {
         LOG.error("Error. RequestId: {}", requestId, exception);
         ErrorResponseDTO errorResponseDTO = errorResponseBuilderService.buildResponse(errorType, requestLocales, requestId);
         errorResponseDTO.setErrorMessage(errorResponseDTO.getErrorMessage() + " Request ID: " + requestId);
+        registerMetrics(errorType, requestId);
         return buildResponse(errorResponseDTO);
     }
 
@@ -53,6 +55,12 @@ public class GeneralExceptionMapper implements ExceptionMapper<Exception> {
         } else {
             return ErrorType.INTERNAL_ERROR;
         }
+    }
+
+    private void registerMetrics(ErrorType errorType, String requestId) {
+        String errorCode = String.valueOf(errorType.getErrorCode());
+        long errorCount = Metrics.ERROR_TYPES_METRIC.get(errorCode).incrementAndGet();
+        LOG.debug("Error code: {} Error count: {} RequestId: {}", errorCode, errorCount, requestId);
     }
 
     private Response buildResponse(ErrorResponseDTO errorResponseDTO) {
