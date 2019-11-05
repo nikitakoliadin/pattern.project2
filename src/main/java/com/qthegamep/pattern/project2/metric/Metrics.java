3731cf1cc7d5c2595057c2fe6a4f2e191a25ef61
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-05 13:22:56 +0200

    Fix code smells

--- src/main/java/com/qthegamep/pattern/project2/metric/Metrics.java
+++ src/main/java/com/qthegamep/pattern/project2/metric/Metrics.java
@@ -7,4 +7,7 @@ public class Metrics {
     public static final AtomicLong TASK_QUEUE_SIZE_METRIC = new AtomicLong();
 
     public static final AtomicLong AVAILABLE_THREADS_METRIC = new AtomicLong(Integer.parseInt(System.getProperty("server.core.pool.size")));
+
+    private Metrics() {
+    }
 }
