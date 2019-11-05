5518371a5134372542bd11ea1a525c5e40e133b7
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-05 13:19:01 +0200

    Created GrizzlyThreadPoolProbe class

---
+++ src/main/java/com/qthegamep/pattern/project2/probe/GrizzlyThreadPoolProbe.java
@@ -0,0 +1,24 @@
+package com.qthegamep.pattern.project2.probe;
+
+import com.qthegamep.pattern.project2.metrics.Metrics;
+import org.glassfish.grizzly.threadpool.AbstractThreadPool;
+import org.glassfish.grizzly.threadpool.ThreadPoolProbe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GrizzlyThreadPoolProbe extends ThreadPoolProbe.Adapter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GrizzlyThreadPoolProbe.class);
+
+    @Override
+    public void onTaskDequeueEvent(AbstractThreadPool threadPool, Runnable task) {
+        long availableThreads = Metrics.AVAILABLE_THREADS_METRIC.decrementAndGet();
+        LOG.debug("Available threads before task {}", availableThreads);
+    }
+
+    @Override
+    public void onTaskCompleteEvent(AbstractThreadPool threadPool, Runnable task) {
+        long availableThreads = Metrics.AVAILABLE_THREADS_METRIC.incrementAndGet();
+        LOG.debug("Available threads after task {}", availableThreads);
+    }
+}
