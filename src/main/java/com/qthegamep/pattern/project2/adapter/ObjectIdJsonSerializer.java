d73c282d09e6d73e1a704a0e680f653dcad52cb7
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-08 17:03:18 +0200

    Created ObjectIdJsonSerializer class

---
+++ src/main/java/com/qthegamep/pattern/project2/adapter/ObjectIdJsonSerializer.java
@@ -0,0 +1,19 @@
+package com.qthegamep.pattern.project2.adapter;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.qthegamep.pattern.project2.util.Constants;
+import org.bson.types.ObjectId;
+
+import java.io.IOException;
+
+class ObjectIdJsonSerializer extends JsonSerializer<ObjectId> {
+
+    @Override
+    public void serialize(ObjectId objectId, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeStartObject(Constants.JSON_OBJECT_ID_FIELD_NAME.getValue());
+        jsonGenerator.writeObjectField(Constants.JSON_OBJECT_ID_FIELD_NAME.getValue(), objectId.toHexString());
+        jsonGenerator.writeEndObject();
+    }
+}
