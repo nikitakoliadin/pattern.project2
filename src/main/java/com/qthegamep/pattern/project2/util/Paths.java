679d09fb9f9c76b1eaa1a8c1368a3334c4f4af3a
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-08 16:59:15 +0200

    Refactored Paths class

--- src/main/java/com/qthegamep/pattern/project2/util/Paths.java
+++ src/main/java/com/qthegamep/pattern/project2/util/Paths.java
@@ -6,5 +6,6 @@ public class Paths {
     public static final String TEST_PATH = "/test";
     public static final String SWAGGER_PATH = "/swagger";
     public static final String DOCS_PATH = "/docs/";
-    public static final String SWAGGER_OPEN_API_PATH = "/swagger/openapi.json";
+    public static final String SWAGGER_OPEN_API_JSON_PATH = "/swagger/openapi.json";
+    public static final String SWAGGER_OPEN_API_YAML_PATH = "/swagger/openapi.yaml";
 }
