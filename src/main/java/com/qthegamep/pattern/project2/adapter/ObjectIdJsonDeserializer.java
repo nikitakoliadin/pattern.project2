a4f47a35a4a48410d6a30eb20c9fb2357a5e1359
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-08 17:02:36 +0200

    Created ObjectIdJsonDeserializer class

---
+++ src/main/java/com/qthegamep/pattern/project2/adapter/ObjectIdJsonDeserializer.java
@@ -0,0 +1,22 @@
+package com.qthegamep.pattern.project2.adapter;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.qthegamep.pattern.project2.util.Constants;
+import org.bson.types.ObjectId;
+
+class ObjectIdJsonDeserializer extends JsonDeserializer<ObjectId> {
+
+    @Override
+    public ObjectId deserialize(JsonParser jsonParser, DeserializationContext context) {
+        try {
+            JsonNode jsonNode = jsonParser.readValueAsTree();
+            String id = jsonNode.get(Constants.JSON_OBJECT_ID_FIELD_NAME.getValue()).asText();
+            return new ObjectId(id);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
