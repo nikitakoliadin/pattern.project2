766d153fab594b359b3b59b3934666354a1fe223
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-14 18:00:54 +0200

    Updated IsoDateJsonSerializer and IsoDateJsonDeserializer classes

--- src/main/java/com/qthegamep/pattern/project2/adapter/IsoDateJsonDeserializer.java
+++ src/main/java/com/qthegamep/pattern/project2/adapter/IsoDateJsonDeserializer.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 class IsoDateJsonDeserializer extends JsonDeserializer<Date> {
 
@@ -17,6 +18,7 @@ class IsoDateJsonDeserializer extends JsonDeserializer<Date> {
     public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         try {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constants.MONGO_UTC_DATE_FORMAT.getValue());
+            simpleDateFormat.setTimeZone(TimeZone.getTimeZone(Constants.GMT_TIMEZONE.getValue()));
             JsonNode jsonNode = jsonParser.readValueAsTree();
             String isoDate = jsonNode.get(Constants.JSON_DATE_FIELD_NAME.getValue()).asText();
             return simpleDateFormat.parse(isoDate);
--- src/main/java/com/qthegamep/pattern/project2/adapter/IsoDateJsonSerializer.java
+++ src/main/java/com/qthegamep/pattern/project2/adapter/IsoDateJsonSerializer.java
@@ -8,12 +8,14 @@ import com.qthegamep.pattern.project2.util.Constants;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 class IsoDateJsonSerializer extends JsonSerializer<Date> {
 
     @Override
     public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constants.MONGO_UTC_DATE_FORMAT.getValue());
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone(Constants.GMT_TIMEZONE.getValue()));
         String isoDate = simpleDateFormat.format(date);
         jsonGenerator.writeStartObject(Constants.JSON_DATE_FIELD_NAME.getValue());
         jsonGenerator.writeObjectField(Constants.JSON_DATE_FIELD_NAME.getValue(), isoDate);
