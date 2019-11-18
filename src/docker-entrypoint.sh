d26d1483de5d57f2dac5a6dabcc9d8f43827668e
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-18 13:25:05 +0200

    New version 1.0.20

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-java -Xms1024m -Xmx2048m -Duser.timezone=Europe/Kiev -Dlog.level=INFO -Dconfig.properties=/opt/srv/conf/config.properties -jar /opt/srv/pattern.project2-1.0.19.jar
+java -Xms1024m -Xmx2048m -Duser.timezone=Europe/Kiev -Dlog.level=INFO -Dconfig.properties=/opt/srv/conf/config.properties -jar /opt/srv/pattern.project2-1.0.20.jar
