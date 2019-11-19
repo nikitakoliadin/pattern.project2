e3429126126179409cbdc41666856eb0fc2c95a8
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-19 10:14:03 +0200

    Implemented saveError method in SyncMongoRepositoryImpl class

--- src/main/java/com/qthegamep/pattern/project2/repository/SyncMongoRepositoryImpl.java
+++ src/main/java/com/qthegamep/pattern/project2/repository/SyncMongoRepositoryImpl.java
@@ -1,6 +1,14 @@
 package com.qthegamep.pattern.project2.repository;
 
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.qthegamep.pattern.project2.entity.Error;
+import com.qthegamep.pattern.project2.exception.SyncMongoRepositoryException;
+import com.qthegamep.pattern.project2.model.ErrorType;
+import com.qthegamep.pattern.project2.service.ConverterService;
+import com.qthegamep.pattern.project2.util.Constants;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,10 +18,31 @@ public class SyncMongoRepositoryImpl implements SyncMongoRepository {
 
     private static final Logger LOG = LoggerFactory.getLogger(SyncMongoRepositoryImpl.class);
 
+    private final String errorCollectionName = System.getProperty("mongodb.error.collection.name");
+
     private MongoDatabase mongoDatabase;
+    private ConverterService converterService;
 
     @Inject
-    public SyncMongoRepositoryImpl(MongoDatabase mongoDatabase) {
+    public SyncMongoRepositoryImpl(MongoDatabase mongoDatabase, ConverterService converterService) {
         this.mongoDatabase = mongoDatabase;
+        this.converterService = converterService;
+    }
+
+    @Override
+    public Error saveError(Error error) throws SyncMongoRepositoryException {
+        try {
+            MongoCollection<Document> errorCollection = mongoDatabase.getCollection(errorCollectionName);
+            String errorJson = converterService.toJson(error);
+            LOG.debug("Sync save error: {} RequestId: {}", errorJson, error.getRequestId());
+            Document query = Document.parse(errorJson);
+            errorCollection.insertOne(query);
+            ObjectId objectId = query.getObjectId(Constants.JSON_OBJECT_ID_KEY.getValue());
+            LOG.debug("ObjectId: {} RequestId: {}", objectId, error.getRequestId());
+            error.setObjectId(objectId);
+            return error;
+        } catch (Exception e) {
+            throw new SyncMongoRepositoryException(e, ErrorType.MONGO_SYNC_SAVE_ERROR_ERROR);
+        }
     }
 }
