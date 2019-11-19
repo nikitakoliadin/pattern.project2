2984aeaaa93d9a8f9c4e6419893993ef0907b51b
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-19 14:24:30 +0200

    Implemented save methods

--- src/main/java/com/qthegamep/pattern/project2/repository/RedisClusterRepositoryImpl.java
+++ src/main/java/com/qthegamep/pattern/project2/repository/RedisClusterRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.qthegamep.pattern.project2.repository;
 
+import com.qthegamep.pattern.project2.exception.RedisRepositoryException;
+import com.qthegamep.pattern.project2.model.ErrorType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.JedisCluster;
@@ -10,10 +12,38 @@ public class RedisClusterRepositoryImpl implements RedisRepository {
 
     private static final Logger LOG = LoggerFactory.getLogger(RedisClusterRepositoryImpl.class);
 
+    private final int defaultTtl = 1200;
+
     private JedisCluster jedisCluster;
 
     @Inject
     public RedisClusterRepositoryImpl(JedisCluster jedisCluster) {
         this.jedisCluster = jedisCluster;
+    }
+
+    @Override
+    public void save(String key, String value) throws RedisRepositoryException {
+        save(key, value, defaultTtl, null);
+    }
+
+    @Override
+    public void save(String key, String value, String requestId) throws RedisRepositoryException {
+        save(key, value, defaultTtl, requestId);
+    }
+
+    @Override
+    public void save(String key, String value, Integer ttl) throws RedisRepositoryException {
+        save(key, value, ttl, null);
+    }
+
+    @Override
+    public void save(String key, String value, Integer ttl, String requestId) throws RedisRepositoryException {
+        LOG.debug("Key: {} Value: {} TTL: {} RequestId: {}", key, value, ttl, requestId);
+        try {
+            jedisCluster.set(key, value);
+            jedisCluster.expire(key, ttl);
+        } catch (Exception e) {
+            throw new RedisRepositoryException(e, ErrorType.REDIS_SAVE_ERROR);
+        }
     }
 }
--- src/main/java/com/qthegamep/pattern/project2/repository/RedisPoolRepositoryImpl.java
+++ src/main/java/com/qthegamep/pattern/project2/repository/RedisPoolRepositoryImpl.java
@@ -1,7 +1,10 @@
 package com.qthegamep.pattern.project2.repository;
 
+import com.qthegamep.pattern.project2.exception.RedisRepositoryException;
+import com.qthegamep.pattern.project2.model.ErrorType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.inject.Inject;
@@ -10,10 +13,38 @@ public class RedisPoolRepositoryImpl implements RedisRepository {
 
     private static final Logger LOG = LoggerFactory.getLogger(RedisPoolRepositoryImpl.class);
 
+    private final int defaultTtl = 1200;
+
     private JedisPool jedisPool;
 
     @Inject
     public RedisPoolRepositoryImpl(JedisPool jedisPool) {
         this.jedisPool = jedisPool;
+    }
+
+    @Override
+    public void save(String key, String value) throws RedisRepositoryException {
+        save(key, value, defaultTtl, null);
+    }
+
+    @Override
+    public void save(String key, String value, String requestId) throws RedisRepositoryException {
+        save(key, value, defaultTtl, requestId);
+    }
+
+    @Override
+    public void save(String key, String value, Integer ttl) throws RedisRepositoryException {
+        save(key, value, ttl, null);
+    }
+
+    @Override
+    public void save(String key, String value, Integer ttl, String requestId) throws RedisRepositoryException {
+        LOG.debug("Key: {} Value: {} TTL: {} RequestId: {}", key, value, ttl, requestId);
+        try (Jedis jedis = jedisPool.getResource()) {
+            jedis.set(key, value);
+            jedis.expire(key, ttl);
+        } catch (Exception e) {
+            throw new RedisRepositoryException(e, ErrorType.REDIS_SAVE_ERROR);
+        }
     }
 }
