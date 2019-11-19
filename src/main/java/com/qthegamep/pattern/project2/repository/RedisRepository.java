7ca1bc8fecd40efa0a4f45de7a6e64376a54cab3
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-19 14:23:05 +0200

    Added save methods to RedisRepository interface

--- src/main/java/com/qthegamep/pattern/project2/repository/RedisRepository.java
+++ src/main/java/com/qthegamep/pattern/project2/repository/RedisRepository.java
@@ -1,4 +1,14 @@
 package com.qthegamep.pattern.project2.repository;
 
+import com.qthegamep.pattern.project2.exception.RedisRepositoryException;
+
 public interface RedisRepository {
+
+    void save(String key, String value) throws RedisRepositoryException;
+
+    void save(String key, String value, String requestId) throws RedisRepositoryException;
+
+    void save(String key, String value, Integer ttl) throws RedisRepositoryException;
+
+    void save(String key, String value, Integer ttl, String requestId) throws RedisRepositoryException;
 }
