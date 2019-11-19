3a841c1db66c793380ae3ce5456a6ca515f6f5a5
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-19 14:26:58 +0200

    Added saveAll methods to RedisRepository interface

--- src/main/java/com/qthegamep/pattern/project2/repository/RedisRepository.java
+++ src/main/java/com/qthegamep/pattern/project2/repository/RedisRepository.java
@@ -2,6 +2,8 @@ package com.qthegamep.pattern.project2.repository;
 
 import com.qthegamep.pattern.project2.exception.RedisRepositoryException;
 
+import java.util.Map;
+
 public interface RedisRepository {
 
     void save(String key, String value) throws RedisRepositoryException;
@@ -11,4 +13,12 @@ public interface RedisRepository {
     void save(String key, String value, Integer ttl) throws RedisRepositoryException;
 
     void save(String key, String value, Integer ttl, String requestId) throws RedisRepositoryException;
+
+    void saveAll(String key, Map<String, String> value) throws RedisRepositoryException;
+
+    void saveAll(String key, Map<String, String> value, String requestId) throws RedisRepositoryException;
+
+    void saveAll(String key, Map<String, String> value, Integer ttl) throws RedisRepositoryException;
+
+    void saveAll(String key, Map<String, String> value, Integer ttl, String requestId) throws RedisRepositoryException;
 }
