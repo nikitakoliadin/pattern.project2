82596735de4244fa1c5743bdf3011013571fc18a
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-19 10:12:11 +0200

    Added saveError method to SyncMongoRepository interface

--- src/main/java/com/qthegamep/pattern/project2/repository/SyncMongoRepository.java
+++ src/main/java/com/qthegamep/pattern/project2/repository/SyncMongoRepository.java
@@ -1,4 +1,9 @@
 package com.qthegamep.pattern.project2.repository;
 
+import com.qthegamep.pattern.project2.entity.Error;
+import com.qthegamep.pattern.project2.exception.SyncMongoRepositoryException;
+
 public interface SyncMongoRepository {
+
+    Error saveError(Error error) throws SyncMongoRepositoryException;
 }
