25b67551a9816915d82c74c4ad53409d8efd214b
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-20 13:46:45 +0200

    Updated CryptoServiceImpl class

--- src/main/java/com/qthegamep/pattern/project2/service/CryptoServiceImpl.java
+++ src/main/java/com/qthegamep/pattern/project2/service/CryptoServiceImpl.java
@@ -2,7 +2,7 @@ package com.qthegamep.pattern.project2.service;
 
 import com.qthegamep.pattern.project2.exception.CryptoServiceException;
 import com.qthegamep.pattern.project2.model.ErrorType;
-import com.qthegamep.pattern.project2.util.Constants;
+import com.qthegamep.pattern.project2.model.HashAlgorithm;
 import org.apache.commons.codec.binary.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,19 +14,27 @@ public class CryptoServiceImpl implements CryptoService {
     private static final Logger LOG = LoggerFactory.getLogger(CryptoServiceImpl.class);
 
     @Override
-    public String encodeToMD5Hash(String data) throws CryptoServiceException {
-        return encodeToMD5Hash(data, null);
+    public String encodeTo(String data, HashAlgorithm hashAlgorithm) throws CryptoServiceException {
+        return encodeTo(data, hashAlgorithm, null);
     }
 
     @Override
-    public String encodeToMD5Hash(String data, String requestId) throws CryptoServiceException {
-        LOG.debug("Encode to MD5 hash: {} RequestId: {}", data, requestId);
+    public String encodeTo(String data, HashAlgorithm hashAlgorithm, String requestId) throws CryptoServiceException {
+        LOG.debug("Encode: {} Algorithm: {} RequestId: {}", data, hashAlgorithm.getAlgorithm(), requestId);
+        if (HashAlgorithm.MD5.equals(hashAlgorithm)) {
+            return encode(data, hashAlgorithm.getAlgorithm());
+        } else {
+            throw new CryptoServiceException(ErrorType.ENCODE_HASH_ALGORITHM_NOT_EXISTS_ERROR);
+        }
+    }
+
+    private String encode(String data, String hashAlgorithm) throws CryptoServiceException {
         try {
-            MessageDigest md5MessageDigest = MessageDigest.getInstance(Constants.MD5_HASH.getValue());
+            MessageDigest md5MessageDigest = MessageDigest.getInstance(hashAlgorithm);
             md5MessageDigest.update(data.getBytes(), 0, data.length());
             return Hex.encodeHexString(md5MessageDigest.digest());
         } catch (Exception e) {
-            throw new CryptoServiceException(e, ErrorType.ENCODE_TO_MD5_HASH_ERROR);
+            throw new CryptoServiceException(e, ErrorType.ENCODE_HASH_ERROR);
         }
     }
 }
