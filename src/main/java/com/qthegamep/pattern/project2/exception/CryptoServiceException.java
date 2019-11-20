2205f25229433cca2c8faea82a780e49768c9309
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-20 13:44:52 +0200

    Updated CryptoServiceException class

--- src/main/java/com/qthegamep/pattern/project2/exception/CryptoServiceException.java
+++ src/main/java/com/qthegamep/pattern/project2/exception/CryptoServiceException.java
@@ -4,6 +4,10 @@ import com.qthegamep.pattern.project2.model.ErrorType;
 
 public class CryptoServiceException extends GeneralServiceException {
 
+    public CryptoServiceException(ErrorType errorType) {
+        super(errorType);
+    }
+
     public CryptoServiceException(Throwable cause, ErrorType errorType) {
         super(cause, errorType);
     }
