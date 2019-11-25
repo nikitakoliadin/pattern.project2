6418d06bed53418dd7bdde117136da4e0a861fed
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-25 16:22:13 +0200

    Fix ApplicationConfigInitializationException class

--- src/main/java/com/qthegamep/pattern/project2/exception/ApplicationConfigInitializationException.java
+++ src/main/java/com/qthegamep/pattern/project2/exception/ApplicationConfigInitializationException.java
@@ -1,6 +1,6 @@
 package com.qthegamep.pattern.project2.exception;
 
-public class ApplicationConfigInitializationException extends Exception {
+public class ApplicationConfigInitializationException extends GeneralServiceInitializationException {
 
     public ApplicationConfigInitializationException(Throwable cause) {
         super(cause);
