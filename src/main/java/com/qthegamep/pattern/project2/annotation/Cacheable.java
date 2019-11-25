85041f0f6d15f8721041354c1c01cde360592f9f
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-25 15:11:23 +0200

    Added keyAlgorithm to Cacheable annotation

--- src/main/java/com/qthegamep/pattern/project2/annotation/Cacheable.java
+++ src/main/java/com/qthegamep/pattern/project2/annotation/Cacheable.java
@@ -1,6 +1,7 @@
 package com.qthegamep.pattern.project2.annotation;
 
 import com.qthegamep.pattern.project2.model.HashAlgorithm;
+import com.qthegamep.pattern.project2.model.KeyAlgorithm;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -10,6 +11,8 @@ import java.lang.annotation.Target;
 @Target(value = {ElementType.METHOD})
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface Cacheable {
+
+    KeyAlgorithm keyAlgorithm() default KeyAlgorithm.FULL_SIGNATURE_WITH_FULL_ARGUMENTS;
 
     HashAlgorithm hashAlgorithm() default HashAlgorithm.MD5;
 }
