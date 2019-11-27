6d8304a9ae7913c75e44958aa5a008acf50d3ce1
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-11-27 14:07:24 +0200

    Created PropertyInjectionResolver class

---
+++ src/main/java/com/qthegamep/pattern/project2/config/PropertyInjectionResolver.java
@@ -0,0 +1,42 @@
+package com.qthegamep.pattern.project2.config;
+
+import com.qthegamep.pattern.project2.annotation.Property;
+import org.glassfish.hk2.api.Injectee;
+import org.glassfish.hk2.api.InjectionResolver;
+import org.glassfish.hk2.api.ServiceHandle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+public class PropertyInjectionResolver implements InjectionResolver<Property> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PropertyInjectionResolver.class);
+
+    @Context
+    private Application application;
+
+    @Override
+    public Object resolve(Injectee injectee, ServiceHandle<?> serviceHandle) {
+        Type requiredType = injectee.getRequiredType();
+        Property annotation = injectee.getParent().getAnnotation(Property.class);
+        Map<String, Object> properties = application.getProperties();
+        String key = annotation.value();
+        Object value = properties.get(key);
+        LOG.debug("Property injection: Required Type: {} Key: {} Value: {}", requiredType, key, value);
+        return value;
+    }
+
+    @Override
+    public boolean isConstructorParameterIndicator() {
+        return false;
+    }
+
+    @Override
+    public boolean isMethodParameterIndicator() {
+        return false;
+    }
+}
