ec295e4ec89a20141613101d3853812631bf21b9
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-12-20 09:38:57 +0200

    Created RedisErrorCounterMetric class

---
+++ src/main/java/com/qthegamep/pattern/project2/metric/RedisErrorCounterMetric.java
@@ -0,0 +1,19 @@
+package com.qthegamep.pattern.project2.metric;
+
+import com.qthegamep.pattern.project2.util.Constants;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
+
+public class RedisErrorCounterMetric implements MeterBinder {
+
+    private static final String REDIS_ERROR_COUNTER = "redis.error.counter";
+
+    @Override
+    public void bindTo(MeterRegistry meterRegistry) {
+        Gauge.builder(REDIS_ERROR_COUNTER, Metrics.REDIS_ERROR_COUNTER_METRIC::get)
+                .description("The current number of Redis errors")
+                .baseUnit(Constants.REDIS.getValue())
+                .register(meterRegistry);
+    }
+}
