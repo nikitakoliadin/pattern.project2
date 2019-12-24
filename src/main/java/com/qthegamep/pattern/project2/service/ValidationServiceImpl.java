4ac8f6f28fe8f23f7ebc0878107b863461c578f2
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-12-24 12:49:06 +0200

    Created ValidationServiceImpl class

---
+++ src/main/java/com/qthegamep/pattern/project2/service/ValidationServiceImpl.java
@@ -0,0 +1,36 @@
+package com.qthegamep.pattern.project2.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.validation.Validation;
+import javax.validation.ValidationException;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.stream.Collectors;
+
+public class ValidationServiceImpl implements ValidationService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ValidationServiceImpl.class);
+
+    private Validator validator;
+
+    public ValidationServiceImpl() {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    @Override
+    public <T> void validate(T object) {
+        if (object != null) {
+            String error = validator.validate(object)
+                    .stream()
+                    .map(value -> String.format("'%s' %s", value.getPropertyPath(), value.getMessage()))
+                    .collect(Collectors.joining("; "));
+            LOG.debug("Validate error: {}", error);
+            if (!error.isEmpty()) {
+                throw new ValidationException(error);
+            }
+        }
+    }
+}
