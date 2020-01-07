5dbc60dbd01bd75c1ea24bad596ab5e8fb634551
Nikita Koliadin <qThegamEp@gmail.com>
2020-01-07 11:45:36 +0200

    Refactored RequestCounterMeter

--- src/main/java/com/qthegamep/pattern/project2/statistics/meter/RequestCounterMeter.java
+++ src/main/java/com/qthegamep/pattern/project2/statistics/meter/RequestCounterMeter.java
@@ -9,15 +9,12 @@ import io.micrometer.core.instrument.binder.MeterBinder;
 
 public class RequestCounterMeter implements MeterBinder {
 
-    private static final String REQUEST_COUNTER = "request.counter";
-    private static final String PATH_TAG = "path";
-
     @Override
     public void bindTo(MeterRegistry meterRegistry) {
-        Meters.REQUEST_COUNTER_METER.forEach((key, value) -> Gauge.builder(REQUEST_COUNTER, value::get)
+        Meters.REQUEST_COUNTER_METER.forEach((key, value) -> Gauge.builder("request.counter", value::get)
                 .description("The request counter")
                 .baseUnit(Constants.GRIZZLY)
-                .tags(Tags.of(PATH_TAG, key))
+                .tags(Tags.of("path", key))
                 .register(meterRegistry));
     }
 }
