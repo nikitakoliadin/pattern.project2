889dba445988457ce56480f62e881ff8bc1d9029
Nikita Koliadin <qThegamEp@gmail.com>
2020-01-07 11:40:10 +0200

    Refactored ErrorTypesMeter

--- src/main/java/com/qthegamep/pattern/project2/statistics/meter/ErrorTypesMeter.java
+++ src/main/java/com/qthegamep/pattern/project2/statistics/meter/ErrorTypesMeter.java
@@ -9,15 +9,12 @@ import io.micrometer.core.instrument.binder.MeterBinder;
 
 public class ErrorTypesMeter implements MeterBinder {
 
-    private static final String ERROR_TYPES = "error.types";
-    private static final String ERROR_CODE_TAG = "error.code";
-
     @Override
     public void bindTo(MeterRegistry meterRegistry) {
-        Meters.ERROR_TYPES_METER.forEach((key, value) -> Gauge.builder(ERROR_TYPES, value::get)
+        Meters.ERROR_TYPES_METER.forEach((key, value) -> Gauge.builder("error.types", value::get)
                 .description("The error response types")
                 .baseUnit(Constants.GRIZZLY)
-                .tags(Tags.of(ERROR_CODE_TAG, key))
+                .tags(Tags.of("error.code", key))
                 .register(meterRegistry));
     }
 }
