4c99a364e93f3a4bcb9680b26256f6f6877d114a
Nikita Koliadin <qThegamEp@gmail.com>
2020-01-07 11:42:58 +0200

    Refactored GrizzlyThreadPoolMeter

--- src/main/java/com/qthegamep/pattern/project2/statistics/meter/GrizzlyThreadPoolMeter.java
+++ src/main/java/com/qthegamep/pattern/project2/statistics/meter/GrizzlyThreadPoolMeter.java
@@ -8,31 +8,25 @@ import io.micrometer.core.instrument.binder.MeterBinder;
 
 public class GrizzlyThreadPoolMeter implements MeterBinder {
 
-    private static final String CORE_POOL_SIZE = "core.pool.size";
-    private static final String MAX_POOL_SIZE = "max.pool.size";
-    private static final String SELECTOR_RUNNERS = "selector.runners";
-    private static final String AVAILABLE_THREADS = "available.threads";
-    private static final String WORKED_THREADS = "worked.threads";
-
     @Override
     public void bindTo(MeterRegistry meterRegistry) {
-        Gauge.builder(CORE_POOL_SIZE, () -> Integer.parseInt(System.getProperty("application.server.core.pool.size")))
+        Gauge.builder("core.pool.size", () -> Integer.parseInt(System.getProperty("application.server.core.pool.size")))
                 .description("The core pool size of the Grizzly server")
                 .baseUnit(Constants.GRIZZLY)
                 .register(meterRegistry);
-        Gauge.builder(MAX_POOL_SIZE, () -> Integer.parseInt(System.getProperty("application.server.max.pool.size")))
+        Gauge.builder("max.pool.size", () -> Integer.parseInt(System.getProperty("application.server.max.pool.size")))
                 .description("The max pool size of the Grizzly server")
                 .baseUnit(Constants.GRIZZLY)
                 .register(meterRegistry);
-        Gauge.builder(SELECTOR_RUNNERS, () -> Runtime.getRuntime().availableProcessors() * Integer.parseInt(System.getProperty("application.server.selector.runners.multiplier")))
+        Gauge.builder("selector.runners", () -> Runtime.getRuntime().availableProcessors() * Integer.parseInt(System.getProperty("application.server.selector.runners.multiplier")))
                 .description("The selector runners of the Grizzly server")
                 .baseUnit(Constants.GRIZZLY)
                 .register(meterRegistry);
-        Gauge.builder(AVAILABLE_THREADS, Meters.AVAILABLE_THREADS_METER::get)
+        Gauge.builder("available.threads", Meters.AVAILABLE_THREADS_METER::get)
                 .description("The current number of available threads in Grizzly thread pool")
                 .baseUnit(Constants.GRIZZLY)
                 .register(meterRegistry);
-        Gauge.builder(WORKED_THREADS, () -> Integer.parseInt(System.getProperty("application.server.core.pool.size")) - Meters.AVAILABLE_THREADS_METER.get())
+        Gauge.builder("worked.threads", () -> Integer.parseInt(System.getProperty("application.server.core.pool.size")) - Meters.AVAILABLE_THREADS_METER.get())
                 .description("The current number of worked threads in Grizzly thread pool")
                 .baseUnit(Constants.GRIZZLY)
                 .register(meterRegistry);
