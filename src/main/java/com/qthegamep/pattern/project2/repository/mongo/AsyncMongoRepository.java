55a17b2ad182862de2298a04356c0c686f96f49f
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2020-01-08 17:30:12 +0200

    Refactored AsyncMongoRepository

--- src/main/java/com/qthegamep/pattern/project2/repository/mongo/AsyncMongoRepository.java
+++ src/main/java/com/qthegamep/pattern/project2/repository/mongo/AsyncMongoRepository.java
@@ -3,6 +3,7 @@ package com.qthegamep.pattern.project2.repository.mongo;
 import com.qthegamep.pattern.project2.model.entity.Error;
 import com.qthegamep.pattern.project2.exception.compile.AsyncMongoRepositoryException;
 
+@FunctionalInterface
 public interface AsyncMongoRepository {
 
     void saveError(Error error) throws AsyncMongoRepositoryException;
--- src/main/java/com/qthegamep/pattern/project2/repository/mongo/AsyncMongoRepositoryImpl.java
+++ src/main/java/com/qthegamep/pattern/project2/repository/mongo/AsyncMongoRepositoryImpl.java
@@ -6,8 +6,6 @@ import com.qthegamep.pattern.project2.binder.property.Property;
 import com.qthegamep.pattern.project2.repository.mongo.callback.AsyncInsertCallback;
 import com.qthegamep.pattern.project2.exception.compile.AsyncMongoRepositoryException;
 import com.qthegamep.pattern.project2.model.entity.Error;
-import com.qthegamep.pattern.project2.service.ConverterService;
-import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,22 +19,18 @@ public class AsyncMongoRepositoryImpl implements AsyncMongoRepository {
     private String errorCollectionName;
 
     private MongoDatabase mongoDatabase;
-    private ConverterService converterService;
 
     @Inject
-    public AsyncMongoRepositoryImpl(MongoDatabase mongoDatabase, ConverterService converterService) {
+    public AsyncMongoRepositoryImpl(MongoDatabase mongoDatabase) {
         this.mongoDatabase = mongoDatabase;
-        this.converterService = converterService;
     }
 
     @Override
     public void saveError(Error error) throws AsyncMongoRepositoryException {
         try {
-            MongoCollection<Document> errorCollection = mongoDatabase.getCollection(errorCollectionName);
-            String errorJson = converterService.toJson(error);
-            LOG.debug("Async save error: {} RequestId: {}", errorJson, error.getRequestId());
-            Document query = Document.parse(errorJson);
-            errorCollection.insertOne(query, new AsyncInsertCallback(error.getRequestId()));
+            MongoCollection<Error> errorCollection = mongoDatabase.getCollection(errorCollectionName, Error.class);
+            LOG.debug("Async save error: {} RequestId: {}", error, error.getRequestId());
+            errorCollection.insertOne(error, new AsyncInsertCallback(error.getRequestId()));
         } catch (Exception e) {
             throw new AsyncMongoRepositoryException(e, com.qthegamep.pattern.project2.model.container.Error.MONGO_ASYNC_SAVE_ERROR_ERROR);
         }
