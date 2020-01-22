a6afc9498fb34eadd5b5dce75cc05b290ad3113f
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2020-01-22 09:45:50 +0200

    Created PropertyInjectionResolverRuntimeException class

---
+++ src/main/java/com/qthegamep/pattern/project2/exception/runtime/PropertyInjectionResolverRuntimeException.java
@@ -0,0 +1,8 @@
+package com.qthegamep.pattern.project2.exception.runtime;
+
+public class PropertyInjectionResolverRuntimeException extends GeneralServiceRuntimeException {
+
+    public PropertyInjectionResolverRuntimeException(String message) {
+        super(message);
+    }
+}
