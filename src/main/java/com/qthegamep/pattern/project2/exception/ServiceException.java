50a4ec862c1d96ece477c8542e01dd8a747dbc21
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2020-01-24 13:38:02 +0200

    Updated ServiceException interface

--- src/main/java/com/qthegamep/pattern/project2/exception/ServiceException.java
+++ src/main/java/com/qthegamep/pattern/project2/exception/ServiceException.java
@@ -2,6 +2,7 @@ package com.qthegamep.pattern.project2.exception;
 
 import com.qthegamep.pattern.project2.model.container.Error;
 
+@FunctionalInterface
 public interface ServiceException {
 
     Error getError();
