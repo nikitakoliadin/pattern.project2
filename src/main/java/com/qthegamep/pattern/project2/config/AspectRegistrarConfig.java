886332f74af247a28f5abed434c3cb97d949fb07
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2020-03-23 14:54:05 +0200

    Fixed error when disable aspects

--- src/main/java/com/qthegamep/pattern/project2/config/AspectRegistrarConfig.java
+++ src/main/java/com/qthegamep/pattern/project2/config/AspectRegistrarConfig.java
@@ -2,6 +2,7 @@ package com.qthegamep.pattern.project2.config;
 
 import com.qthegamep.pattern.project2.cache.CacheAspect;
 import org.aspectj.lang.Aspects;
+import org.aspectj.lang.NoAspectBoundException;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +14,11 @@ public class AspectRegistrarConfig {
     private final boolean registerCacheAspect = Boolean.parseBoolean(System.getProperty("aop.register.cache.aspect"));
 
     public void register(ResourceConfig resourceConfig) {
-        registerCacheAspect(resourceConfig);
+        try {
+            registerCacheAspect(resourceConfig);
+        } catch (NoAspectBoundException e) {
+            LOG.warn("Aspects disabled!");
+        }
     }
 
     private void registerCacheAspect(ResourceConfig resourceConfig) {
