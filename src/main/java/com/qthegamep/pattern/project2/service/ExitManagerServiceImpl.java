5ecf5fdae6caa1b72628a6af09978fcd250f3f50
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2020-05-13 11:14:33 +0300

    Updated ExitManagerServiceImpl

--- src/main/java/com/qthegamep/pattern/project2/service/ExitManagerServiceImpl.java
+++ src/main/java/com/qthegamep/pattern/project2/service/ExitManagerServiceImpl.java
@@ -3,13 +3,22 @@ package com.qthegamep.pattern.project2.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
+
 public class ExitManagerServiceImpl implements ExitManagerService {
 
     private static final Logger LOG = LoggerFactory.getLogger(ExitManagerServiceImpl.class);
 
+    private Runtime runtime;
+
+    @Inject
+    public ExitManagerServiceImpl(Runtime runtime) {
+        this.runtime = runtime;
+    }
+
     @Override
     public void exit(int exitCode) {
         LOG.warn("Exit with code: {}", exitCode);
-        System.exit(exitCode);
+        runtime.exit(exitCode);
     }
 }
